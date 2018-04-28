9f8eed0dd8c37d0ee67bb134cde23eb4f0773995
Daniil Yarmalkevich <ydpl1998@gmail.com>
2018-04-28 13:28:42 +0300

    class of polynomial function

---
+++ src/additive/Polynom.java
@@ -0,0 +1,33 @@
+package additive;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.function.Function;
+
+public class Polynom implements Function<Float, Float> {
+
+    private ArrayList<Float> a;
+
+    public Polynom(ArrayList<Float> a) {
+        this.a = a;
+    }
+
+    @Override
+    public Float apply(Float x) {
+        LinkedList<Float> list=new LinkedList<>();
+        for (int i = 0; i < a.size(); i++) {
+            float start=a.get(i);
+            for (int j = 0; j < i; j++) {
+                start*=x;
+            }
+            list.add(start);
+        }
+        return (float)list.stream().mapToDouble(hey->hey).sum();
+    }
+
+    @Override
+    public String toString() {
+
+        return "Polynom:";
+    }
+}
