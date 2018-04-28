9aa09f76f1f23f236ea909c1c8af71ce598731ef
Daniil Yarmalkevich <ydpl1998@gmail.com>
2018-04-28 12:13:35 +0300

    moved common methods to new class

--- src/task1/NewtonChebyshevInterpolator.java
+++ src/task1/NewtonChebyshevInterpolator.java
@@ -1,14 +1,16 @@
 package task1;
 
+import additive.CommonAnalysis;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.function.Function;
 
-import static java.lang.Math.*;
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
 
 public class NewtonChebyshevInterpolator {
 
@@ -17,7 +19,6 @@ public class NewtonChebyshevInterpolator {
     private Function<Float, Float> nthDerivative;
     private float a;
     private float b;
-    private float[][] table;
     private ArrayList<Float> nodeList;
     private NewtonPolynom polynom;
     private float bestMistake;
@@ -25,15 +26,15 @@ public class NewtonChebyshevInterpolator {
 
     public static void main(String[] args) {
         ArrayList<Integer> m = new ArrayList<>(Arrays.asList(4, 5, 6, 8, 9));
-        HashMap<Integer, Function<Float, Float>> derivatives=new HashMap<>();
-        derivatives.put(4, x->  (float) (-15*(64000*PI*PI*PI+14400*PI*PI*x+1680*PI*x*x+77*x*x*x)/(128*pow(5*PI+x, 17./4))));
-        derivatives.put(5, x-> (float) (75*(160000*PI*PI*PI+24000*PI*PI*x+2100*PI*x*x+77*x*x*x)/(512*pow(5*PI+x, 21./4))));
-        derivatives.put(6, x-> (float) (-675*(320000*PI*PI*PI+36000*PI*PI*x+2520*PI*x*x+77*x*x*x)/(2048*pow(5*PI+x, 25./4))));
-        derivatives.put(8, x-> (float) (-1044225*(128000*PI*PI*PI+9600*PI*PI*x+480*PI*x*x+11*x*x*x)/(32768*pow(5*PI+x, 33./4))));
-        derivatives.put(9, x-> (float) (3132675*(1344000*PI*PI*PI+86400*PI*PI*x+3780*PI*x*x+77*x*x*x)/(131072*pow(5*PI+x, 37./4))));
+        HashMap<Integer, Function<Float, Float>> derivatives = new HashMap<>();
+        derivatives.put(4, x -> (float) (-15 * (64000 * PI * PI * PI + 14400 * PI * PI * x + 1680 * PI * x * x + 77 * x * x * x) / (128 * pow(5 * PI + x, 17. / 4))));
+        derivatives.put(5, x -> (float) (75 * (160000 * PI * PI * PI + 24000 * PI * PI * x + 2100 * PI * x * x + 77 * x * x * x) / (512 * pow(5 * PI + x, 21. / 4))));
+        derivatives.put(6, x -> (float) (-675 * (320000 * PI * PI * PI + 36000 * PI * PI * x + 2520 * PI * x * x + 77 * x * x * x) / (2048 * pow(5 * PI + x, 25. / 4))));
+        derivatives.put(8, x -> (float) (-1044225 * (128000 * PI * PI * PI + 9600 * PI * PI * x + 480 * PI * x * x + 11 * x * x * x) / (32768 * pow(5 * PI + x, 33. / 4))));
+        derivatives.put(9, x -> (float) (3132675 * (1344000 * PI * PI * PI + 86400 * PI * PI * x + 3780 * PI * x * x + 77 * x * x * x) / (131072 * pow(5 * PI + x, 37. / 4))));
         m.forEach(M -> {
             try {
-                NewtonChebyshevInterpolator interpolator = new NewtonChebyshevInterpolator(x -> (float) (10*x*x*x*Math.pow(Math.PI*5+x,-0.25)),
+                NewtonChebyshevInterpolator interpolator = new NewtonChebyshevInterpolator(x -> (float) (10 * x * x * x * Math.pow(Math.PI * 5 + x, -0.25)),
                         0, 10, M,
                         derivatives.get(M));
                 interpolator.createOutput();
@@ -56,43 +57,16 @@ public class NewtonChebyshevInterpolator {
         processData();
     }
 
-    public static ArrayList<Float> chebyshevNodes(float a,
-                                                  float b,
-                                                  int m,
-                                                  Function<Float, Float> f) {
-        ArrayList<Float> list = new ArrayList<>(m);
-        for (int i = 0; i < m; i++) {
-            float result = (float) ((b + a) / 2 + (b - a) / 2 * cos(PI * (2 * i + 1) / (2 * m)));
-            list.add(result);
-        }
-        Collections.reverse(list);
-        return list;
-    }
-
-    public static float[][] table(ArrayList<Float> x,
-                                  Function<Float, Float> f) {
-        float[][] result = new float[x.size()][x.size()];
-        for (int i = 0; i < x.size(); i++) {
-            result[0][i] = f.apply(x.get(i));
-        }
-        for (int i = 1; i < x.size(); i++) {
-            for (int j = i; j < x.size(); j++) {
-                result[i][j] = (result[i - 1][j] - result[i - 1][j - 1]) / (x.get(j) - x.get(j - i));
-            }
-        }
-        return result;
-    }
-
     private void processData() {
-        nodeList = chebyshevNodes(a, b, m, f);
-        table = table(nodeList, f);
+        nodeList = CommonAnalysis.chebyshevNodes(a, b, m);
+        float[][] table = CommonAnalysis.table(nodeList, f);
         ArrayList<Float> coefficients = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             coefficients.add(table[i][i]);
         }
         polynom = new NewtonPolynom(coefficients, nodeList);
-        bestMistake = bestMistake(a, b, m, nthDerivative);
-        mistakes=mistake(a,b,m,nthDerivative);
+        bestMistake = CommonAnalysis.bestMistake(a, b, m, nthDerivative);
+        mistakes = CommonAnalysis.mistake(a, b, m, nthDerivative);
     }
 
     public void createOutput() throws IOException {
@@ -107,60 +81,9 @@ public class NewtonChebyshevInterpolator {
             writer.printf("%.7f", polynom.apply(X));
             writer.println();
         });
-        writer.println("Best mistake: "+bestMistake);
+        writer.println("Best mistake: " + bestMistake);
         writer.print("Mistakes: ");
-        mistakes.forEach(x -> writer.print(x+" "));
+        mistakes.forEach(x -> writer.print(x + " "));
         writer.close();
-    }
-
-    public static float bestMistake(float a,
-                                    float b,
-                                    int n,
-                                    Function<Float, Float> nthDerivative) {
-        return (float) (maxAbsValue(a, b, nthDerivative, 0.01f) * pow(b - a, n) * pow(2, 1 - 2 * n) / factorial(n));
-    }
-
-    private static int factorial(int n) {
-        int result = 1;
-        int next = 2;
-        while (next <= n) {
-            result *= next;
-            next += 1;
-        }
-        return result;
-    }
-
-    private static float maxAbsValue(float a,
-                                     float b,
-                                     Function<Float, Float> f,
-                                     float interval) {
-        float result = 0;
-        for (float i = a; i <= b; i += interval) {
-            float newResult = Math.abs(f.apply(i));
-            if (newResult > result) {
-                result = newResult;
-            }
-        }
-        return result;
-    }
-
-    private static ArrayList<Float> mistake(float a,
-                                            float b,
-                                            int m,
-                                            Function<Float,Float> nthDerivative) {
-        float h = (b - a) / (m - 1);
-        ArrayList<Float> mistakes=new ArrayList<>();
-        for (int i = 0; i < m - 1; i++) {
-            float point = (float) ((b - a) * 0.5 * (2 * (float)i + 1) / ((float)m - 1));
-            float t = (point - a) / h;
-            float mistake = maxAbsValue(a,b,nthDerivative,0.01f);
-            for (int j = 0; j < m; j++) {
-                mistake *= (t - j);
-            }
-            mistake = (float) (Math.abs(mistake) * pow(h, m) / factorial(m));
-            mistakes.add(mistake);
-        }
-        return mistakes;
-
     }
 }
