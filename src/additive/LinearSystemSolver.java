9457fb59216025ebc8c18b63794552e61c1dd8f2
Daniil Yarmalkevich <ydpl1998@gmail.com>
2018-04-29 14:54:20 +0300

    started implementing Householder

--- src/additive/LinearSystemSolver.java
+++ src/additive/LinearSystemSolver.java
@@ -1,8 +1,37 @@
 package additive;
 
+import com.sun.istack.internal.NotNull;
+
 import java.util.ArrayList;
 
 public class LinearSystemSolver {
+
+    //toDo
+    public static ArrayList<Float> solveHouseholder(float[][] squareMatrix, float[] column) {
+        int n=squareMatrix.length;
+        float[][] matrix=new float[n][n];
+        float[] b=new float[n];
+        for (int i = 0; i < n; i++) {
+            b[i]=column[i];
+            System.arraycopy(squareMatrix[i], 0, matrix[i], 0, n);
+        }
+        float[] diagonalElements=new float[n];
+        for (int i = 0; i < n; i++) {
+            float[] e=new float[n-i];
+            e[0]=1;
+            float normS=columnNorm(matrix,i,i);
+            float[] w=new float[n-i];
+            if (matrix[i][i]!=0) {
+                w[i]=1f/(matrix[i][i]+Math.signum(matrix[i][i])*normS);
+            } else {
+                w[i]=1f/(matrix[i][i]+normS);
+            }
+            for (int j = i; j < n; j++) {
+
+            }
+        }
+        return null;
+    }
 
     public static ArrayList<Float> solveGauss(float[][] squareMatrix, float[] column) {
         int n=squareMatrix.length;
@@ -51,7 +80,7 @@ public class LinearSystemSolver {
         return x;
     }
 
-    private static int indexMaxInCol(float[][] matrix, int column) {
+    private static int indexMaxInCol(@NotNull float[][] matrix, int column) {
         int result = column;
         for (int i = column + 1; i < matrix.length; ++i) {
             if (Math.abs(matrix[i][column]) > Math.abs(matrix[result][column])) {
@@ -59,6 +88,54 @@ public class LinearSystemSolver {
             }
         }
         return result;
+    }
+
+    private static float[] sum(@NotNull float[] p1, @NotNull float[] p2) {
+        float[] result=new float[p1.length];
+        for (int i = 0; i < p1.length; i++) {
+            result[i]=p1[i]+p2[i];
+        }
+        return result;
+    }
+
+    private static float[]  multiply(@NotNull float[] p1, float a) {
+        float[] result=new float[p1.length];
+        for (int i = 0; i < p1.length; i++) {
+            result[i]=p1[i]*a;
+        }
+        return result;
+    }
+
+    private static float norm(@NotNull float[] p) {
+        float result=0;
+        for (int i = 0; i < p.length; i++) {
+            result+=p[i]*p[i];
+        }
+        return (float) Math.sqrt(result);
+    }
+
+    private static float[] getColumn(@NotNull float[][] matrix, int j) {
+        float[] result=new float[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            result[i]=matrix[i][j];
+        }
+        return result;
+    }
+
+    private static float columnNorm(float[][] matrix, int j) {
+        float result=0;
+        for (int i = 0; i < matrix.length; i++) {
+            result+=matrix[i][j]*matrix[i][j];
+        }
+        return (float) Math.sqrt(result);
+    }
+
+    private static float columnNorm(float[][] matrix, int j, int k) {
+        float result=0;
+        for (int i = k; i < matrix.length; i++) {
+            result+=matrix[i][j]*matrix[i][j];
+        }
+        return (float) Math.sqrt(result);
     }
 
 }
