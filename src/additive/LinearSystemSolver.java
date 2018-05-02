d0402ca3d5c7a8827865b7e92d7f612dac5ac0ae
Daniil <ydpl1998@gmail.com>
2018-05-02 21:13:32 +0300

    finished householder's method

--- src/additive/LinearSystemSolver.java
+++ src/additive/LinearSystemSolver.java
@@ -3,10 +3,10 @@ package additive;
 import com.sun.istack.internal.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LinearSystemSolver {
 
-    //toDo
     public static ArrayList<Float> solveHouseholder(float[][] squareMatrix, float[] column) {
         int n=squareMatrix.length;
         float[][] matrix=new float[n][n];
@@ -16,24 +16,57 @@ public class LinearSystemSolver {
             System.arraycopy(squareMatrix[i], 0, matrix[i], 0, n);
         }
         float[] diagonalElements=new float[n];
-        for (int i = 0; i < n; i++) {
-            float[] e=new float[n-i];
-            e[0]=1;
-            float normS=columnNorm(matrix,i,i);
-            float[] w=new float[n-i];
-            if (matrix[i][i]!=0) {
-                w[i]=1f/(matrix[i][i]+Math.signum(matrix[i][i])*normS);
-            } else {
-                w[i]=1f/(matrix[i][i]+normS);
-            }
-            for (int j = i; j < n; j++) {
-
+        for (int k = 0; k < n; k++) {
+            float norm=columnNorm(matrix,k,k);
+            if (norm!=0f) {
+                if (matrix[k][k] < 0) {
+                    norm = -norm;
+                }
+                diagonalElements[k]=-norm;
+                for (int i = k; i < n; i++) {
+                    matrix[i][k]/=norm;
+                }
+                matrix[k][k]=matrix[k][k]+1f;
+                for (int j = k+1; j < n; j++) {
+                    float s = 0f;
+                    for (int i = k; i < n; i++) {
+                        s += matrix[i][k]*matrix[i][j];
+                    }
+                    s = -s/matrix[k][k];
+                    for (int i = k; i < n; i++) {
+                        matrix[i][j] += s*matrix[i][k];
+                    }
+                }
             }
         }
-        return null;
+        for (int k = 0; k < n; k++) {
+                double s = 0.0;
+                for (int i = k; i < n; i++) {
+                    s += matrix[i][k]*b[i];
+                }
+                s = -s/matrix[k][k];
+                for (int i = k; i < n; i++) {
+                    b[i] += s*matrix[i][k];
+                }
+
+        }
+        for (int k = n-1; k >= 0; k--) {
+
+                b[k] /= diagonalElements[k];
+
+            for (int i = 0; i < k; i++) {
+
+                    b[i] -= b[k]*matrix[i][k];
+            }
+        }
+        ArrayList<Float> x=new ArrayList<>(n);
+        for (float v : b) {
+            x.add(v);
+        }
+        return x;
     }
 
-    public static ArrayList<Float> solveGauss(float[][] squareMatrix, float[] column) {
+    public static ArrayList<Float> solveGauss(@NotNull float[][] squareMatrix,@NotNull float[] column) {
         int n=squareMatrix.length;
         float[][] matrix=new float[n][n];
         float[] b=new float[n];
@@ -109,9 +142,9 @@ public class LinearSystemSolver {
     private static float norm(@NotNull float[] p) {
         float result=0;
         for (int i = 0; i < p.length; i++) {
-            result+=p[i]*p[i];
+            result=hypot(result,p[i]);
         }
-        return (float) Math.sqrt(result);
+        return result;
     }
 
     private static float[] getColumn(@NotNull float[][] matrix, int j) {
@@ -122,20 +155,34 @@ public class LinearSystemSolver {
         return result;
     }
 
-    private static float columnNorm(float[][] matrix, int j) {
+    private static float columnNorm(@NotNull float[][] matrix, int j) {
         float result=0;
         for (int i = 0; i < matrix.length; i++) {
-            result+=matrix[i][j]*matrix[i][j];
+            result=hypot(result,matrix[i][j]);
         }
-        return (float) Math.sqrt(result);
+        return result;
     }
 
-    private static float columnNorm(float[][] matrix, int j, int k) {
+    private static float columnNorm(@NotNull float[][] matrix, int j, int k) {
         float result=0;
         for (int i = k; i < matrix.length; i++) {
-            result+=matrix[i][j]*matrix[i][j];
+            result=hypot(result,matrix[i][j]);
         }
-        return (float) Math.sqrt(result);
+        return result;
+    }
+
+    private static float hypot(float a, float b) {
+        float result;
+        if (Math.abs(a) > Math.abs(b)) {
+            result = b/a;
+            result = (float) (Math.abs(a)*Math.sqrt(1+result*result));
+        } else if (b != 0) {
+            result = a/b;
+            result = (float) (Math.abs(b)*Math.sqrt(1+result*result));
+        } else {
+            result = 0f;
+        }
+        return result;
     }
 
 }
