9efeacb8c54426042aedb50a784345f054dce16c
Daniil Yarmalkevich <ydpl1998@gmail.com>
2018-05-03 18:24:07 +0300

    added documentation

--- src/task1/NewtonPolynom.java
+++ src/task1/NewtonPolynom.java
@@ -4,16 +4,35 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.function.Function;
 
+/**
+ * Class of Newton polynomial function, that is defined by list of nodes and list of divided differences
+ */
 public class NewtonPolynom implements Function<Float, Float> {
 
+    /**
+     * List of divided differences
+     */
     private ArrayList<Float> a;
+    /**
+     * List of nodes
+     */
     private ArrayList<Float> nodes;
 
+    /**
+     * Constructs Newton polynomial function
+     * @param a list of divided differences
+     * @param nodes list of nodes
+     */
     public NewtonPolynom(ArrayList<Float> a, ArrayList<Float> nodes) {
         this.a = a;
         this.nodes = nodes;
     }
 
+    /**
+     * Calculates the value of polynomial in point
+     * @param x point
+     * @return numerical value of <i>P(x)</i>
+     */
     @Override
     public Float apply(Float x) {
         LinkedList<Float> list=new LinkedList<>();
