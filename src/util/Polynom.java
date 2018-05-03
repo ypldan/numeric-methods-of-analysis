7198a30fa4f0f66aa024359e0225f18da5ed5875
Daniil Yarmalkevich <ydpl1998@gmail.com>
2018-05-03 18:23:46 +0300

    added documentation

--- src/util/Polynom.java
+++ src/util/Polynom.java
@@ -4,14 +4,30 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.function.Function;
 
+/**
+ * Class of polynomial function
+ */
 public class Polynom implements Function<Float, Float> {
 
+    /**
+     * Polynomial coefficients
+     */
     private ArrayList<Float> a;
 
+    /**
+     * Constructs polynomial function with its coefficients
+     * @param a list of coefficients, <i>P<sub>n</sub>(x)=a<sub>0</sub>+
+     *          a<sub>1</sub>x<sup>1</sup>+...+a<sub>n</sub>x<sup>n</sup></i>
+     */
     public Polynom(ArrayList<Float> a) {
         this.a = a;
     }
 
+    /**
+     * Find value of polynom in point
+     * @param x point
+     * @return value P(x)
+     */
     @Override
     public Float apply(Float x) {
         LinkedList<Float> list=new LinkedList<>();
@@ -23,11 +39,5 @@ public class Polynom implements Function<Float, Float> {
             list.add(start);
         }
         return (float)list.stream().mapToDouble(hey->hey).sum();
-    }
-
-    @Override
-    public String toString() {
-
-        return "Polynom:";
     }
 }
